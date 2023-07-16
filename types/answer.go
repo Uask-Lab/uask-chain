c8b4655c56fee98ef12bfccb4743bc4cb79cad14
xinran chen <lawliet@xinran-m1x.local>
2023-07-16 15:35:02 +0800

    timestamp int, delete recommander

--- types/answer.go
+++ types/answer.go
@@ -1,17 +1,14 @@
 package types
 
-import "github.com/yu-org/yu/common"
-
 type AnswerInfo struct {
 	AnswerUpdateRequest
 }
 
 type AnswerAddRequest struct {
 	// question id
-	QID         string          `json:"qid"`
-	Content     []byte          `json:"content"`
-	Timestamp   string          `json:"timestamp"`
-	Recommender *common.Address `json:"recommender,omitempty"`
+	QID       string `json:"qid"`
+	Content   []byte `json:"content"`
+	Timestamp int    `json:"timestamp"`
 }
 
 type AnswerUpdateRequest struct {
@@ -21,10 +18,9 @@ type AnswerUpdateRequest struct {
 
 // AnswerScheme stores into statedb
 type AnswerScheme struct {
-	ID          string `json:"id" gorm:"primaryKey;column:id"`
-	QID         string `json:"qid" gorm:"column:qid"`
-	FileHash    string `json:"file_hash" gorm:"column:file_hash"`
-	Answerer    string `json:"answerer" gorm:"column:answerer"`
-	Timestamp   string `json:"timestamp" gorm:"column:timestamp"`
-	Recommender string `json:"recommender" gorm:"column:recommender"`
+	ID        string `json:"id" gorm:"primaryKey;column:id"`
+	QID       string `json:"qid" gorm:"column:qid"`
+	FileHash  string `json:"file_hash" gorm:"column:file_hash"`
+	Answerer  string `json:"answerer" gorm:"column:answerer"`
+	Timestamp int    `json:"timestamp" gorm:"column:timestamp"`
 }
